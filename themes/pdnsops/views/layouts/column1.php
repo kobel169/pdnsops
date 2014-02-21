f85dc0800e0b8a33531269b0a9983ef5f2483fc4
Nikita Shapovalov <kobel169@gmail.com>
2014-02-21 08:11:32 +0400

    Deleted pdnsops theme

--- themes/pdnsops/views/layouts/column1.php
+++
@@ -1,6 +0,0 @@
-<?php $this->beginContent('//layouts/main'); ?>
-<div class="content" style="float: left; width: 950px;">
-        <?php echo $content; ?>
-</div><!-- end .content -->
-<?php $this->endContent(); ?>
-
